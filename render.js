5561d890f71eb60e1e34960a39ac6f8acaadf3c0
Kevin Johansmeyer <kevinjohansmeyer@gmail.com>
2023-08-28 16:48:23 -0400

    First commit, working but needs layout changes

---
+++ render.js
@@ -0,0 +1,282 @@
+//=============================================================================//
+// --------------------------------- Header ---------------------------------- //
+//=============================================================================//
+//                      Created by Kevin Johansmeyer                           //
+//                    Email: kevinjohansmeyer@gmail.com                        //
+//=============================================================================//
+
+'use strict'
+
+//=============================================================================//
+// ----------------------------- Update function ----------------------------- //
+//=============================================================================//
+// This entire function updates every time a slider is changed
+function updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal) {
+
+    //=============================================================================//
+    // ------------------------------ Calculations ------------------------------- //
+    //=============================================================================//
+
+    //-------------------------------- Time Array ---------------------------------//
+    var deltat = 0.01,
+        N = timeMaxSliderVal/deltat;
+    
+    var t = new Array(N).fill(0); //probably can define with time steps instead of defining with zeros
+
+    t[0] = 0; //fills t array with [0, deltat, 2*deltat, 3*deltat...]
+    for (let i = 1; i < N; i++) {
+        t[i] = t[i - 1] + deltat;
+    }
+
+    //----------------------------- Defining Arrays -----------------------------//
+    var positionValues = new Array(N).fill(0),
+        velocityValues = new Array(N).fill(0),
+        accelerationValues = new Array(N).fill(0);
+
+    for (let n = 0; n < N; n++) {
+        positionValues[n] = initialPositionSliderVal + initialVelocitySliderVal*t[n] + (1/2)*constantAccelerationSliderVal*Math.pow(t[n], 2),
+        velocityValues[n] = initialVelocitySliderVal + constantAccelerationSliderVal*t[n],
+        accelerationValues[n] = constantAccelerationSliderVal;
+    }
+
+    //=============================================================================//
+    // ----------------------------- Generating Plots ---------------------------- //
+    //=============================================================================//
+    // ----------------------- Position vs. Time Plot ---------------------- //
+    let layout0 = {
+        title: {text: 'Position vs. Time', font: {family: 'Helvetica', size: 32, color: 'white'}},
+        xaxis: {
+            title: {
+                text: 'Time (sec)',
+                font: {family: 'Helvetica', size: 26,color: 'white'}
+            },
+            tickfont: {family: 'Helvetica', size: 18, color: 'white'},
+            color: 'white',
+            rangemode: 'nonnegative', // does this work?
+            showgrid: false,
+            ticks: 'outside'
+        },
+        yaxis: {
+            title: {
+                text: 'Position (m)',
+                font: {family: 'Helvetica', size: 26,color: 'white'}
+            },
+            tickfont: {family: 'Helvetica', size: 18,color: 'white'},
+            color: 'white',
+            showgrid: false,
+            ticks: 'outside'
+            // range: [-hMax, hMax]
+        },
+        shapes: [ //Horizontal line for h vs. t plot
+            {
+                type: 'line',
+                xref: 'x',
+                x0: 0,
+                y0: -10,
+                x1: 0,
+                y1: 10,
+                line:{
+                    color: 'black',
+                    width: 1,
+                    dash:'solid'
+                }
+            }
+        ],
+        margin: {l: 100, r: 50, b: 60, t: 75, pad: 4},
+        plot_bgcolor: 'white', //"#383838",
+        paper_bgcolor: '#181818'
+    }
+
+    let trace0 = {
+        x: t,
+        y: positionValues,
+        name: 'Position vs. Time',
+        type: 'scatter',
+        line: {
+            color: '#ff3d3d',//'#282828',
+            width: 3,
+            shape: 'spline', // Spline used to smooth curve between points
+            smoothing: 1.3 // Smoothing value between 0 and 1
+          }
+    };
+
+    var config0 = {
+        toImageButtonOptions: {
+          format: 'png', // one of png, svg, jpeg, webp
+          filename: 'positionVsTimePlot',
+          height: 500,
+          width: 1754,
+          scale: 2 // Multiply title/legend/axis/canvas sizes by this factor
+        },
+        modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']
+    };
+
+    let data0 = [trace0];
+
+    Plotly.newPlot('positionVsTimePlot', data0, layout0, config0);
+    
+    // ----------------------- Velocity vs. Time Plot ---------------------- //
+    let layout1 = {
+        title: {text: 'Velocity vs. Time', font: {family: 'Times New Roman', size: 32, color: 'white'}},
+        xaxis: {
+            title: {text: 'Time (sec)', font: {family: 'Times New Roman', size: 26, color: 'white'}},
+            color: 'white',
+            showgrid: false,
+            ticks: 'outside',
+            tickfont: {family: 'Times New Roman', size: 18,color: 'white'},
+        },
+        yaxis: {
+            title: {text: 'Velocity (m/s)', font: {family: 'Times New Roman', size: 26, color: 'white'}},
+            color: 'white',
+            showgrid: false,
+            ticks: 'outside',
+            tickfont: {family: 'Times New Roman', size: 18,color: 'white'},
+        },
+        margin: {l: 100, r: 50, b: 60, t: 75, pad: 4},
+        plot_bgcolor: 'white', //"#383838",
+        paper_bgcolor: "#181818"
+    }
+
+    let trace1 = {
+        x: t,
+        y: velocityValues,
+        type: 'scatter',
+        line: {
+            color: '#ff3d3d',
+            width: 3
+          }
+    };
+
+    var config1 = {
+        toImageButtonOptions: {
+          format: 'png', // one of png, svg, jpeg, webp
+          filename: 'velocityVsTimePlot',
+          height: 500,
+          width: 1754,
+          scale: 2 // Multiply title/legend/axis/canvas sizes by this factor
+        },
+        modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']
+    };
+
+    let data1 = [trace1];
+    Plotly.newPlot('velocityVsTimePlot', data1, layout1, config1, {modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']});
+
+    // ----------------------- Acceleration vs. Time Plot ---------------------- //
+    let layout2 = {
+        title: {text: 'Accleration vs. Time', font: {family: 'Times New Roman', size: 32, color: 'white'}},
+        xaxis: {
+            title: {text: 'Time (sec)', font: {family: 'Times New Roman', size: 26, color: 'white'}},
+            color: 'white',
+            showgrid: false,
+            ticks: 'outside',
+            tickfont: {family: 'Times New Roman', size: 18,color: 'white'},
+        },
+        yaxis: {
+            title: {text: 'Acceleration (m/s^2)', font: {family: 'Times New Roman', size: 26, color: 'white'}},
+            color: 'white',
+            showgrid: false,
+            ticks: 'outside',
+            tickfont: {family: 'Times New Roman', size: 18,color: 'white'},
+        },
+        margin: {l: 100, r: 50, b: 60, t: 75, pad: 4},
+        plot_bgcolor: 'white', //"#383838",
+        paper_bgcolor: "#181818"
+    }
+
+    let trace2 = {
+        x: t,
+        y: accelerationValues,
+        type: 'scatter',
+        line: {
+            color: '#ff3d3d',
+            width: 3
+            }
+    };
+
+    var config2 = {
+        toImageButtonOptions: {
+            format: 'png', // one of png, svg, jpeg, webp
+            filename: 'accelerationVsTime',
+            height: 500,
+            width: 1754,
+            scale: 2 // Multiply title/legend/axis/canvas sizes by this factor
+        },
+        modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']
+    };
+
+    let data2 = [trace2];
+    Plotly.newPlot('accelerationVsTimePlot', data2, layout2, config2, {modeBarButtonsToRemove: ['autoScale2d','toggleSpikelines','hoverClosestCartesian','hoverCompareCartesian']});
+    
+    console.log({initialPositionSliderVal});
+    console.log({initialVelocitySliderVal});
+    console.log({constantAccelerationSliderVal});
+    console.log({timeMaxSliderVal});
+
+} // ----------------------- End of Update Function ---------------------- //
+
+// ----------------------------- UI Elements ----------------------------- //
+const initialPositionSlider = document.getElementById("initialPositionSlider");
+const initialVelocitySlider = document.getElementById("initialVelocitySlider");
+const constantAccelerationSlider = document.getElementById("constantAccelerationSlider");
+const timeMaxSlider = document.getElementById("timeMaxSlider");
+
+var initialPositionSliderVal = Number(initialPositionSlider.value),
+    initialVelocitySliderVal = Number(initialVelocitySlider.value),
+    constantAccelerationSliderVal = Number(constantAccelerationSlider.value),
+    timeMaxSliderVal = Number(timeMaxSlider.value);
+
+
+// ----------------------------- Update Slider Values ----------------------------- //
+initialPositionSlider.addEventListener('change', function (event) {
+    initialPositionSliderVal = Number(initialPositionSlider.value);
+    updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal);
+})
+
+initialVelocitySlider.addEventListener('change', function (event) {
+    initialVelocitySliderVal = Number(initialVelocitySlider.value);
+    updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal);
+})
+
+constantAccelerationSlider.addEventListener('change', function (event) {
+    constantAcceleration = Number(constantAccelerationSlider.value);
+    updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal);
+})
+
+timeMaxSlider.addEventListener('change', function (event) {
+    timeMaxSliderVal = Number(initialPositionSlider.value);
+    updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal);
+})
+// --------------------------- Toggle Plots --------------------------- //
+//Citation: https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
+function togglePositionVsTimePlot() {
+    var x = document.getElementById("positionVsTimePlot");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+function toggleVelocityVsTimePlot() {
+    var x = document.getElementById("velocityVsTimePlot");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function toggleAccelerationVsTimePlot() {
+    var x = document.getElementById("accelerationVsTimePlot");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+// ------------------ Execute update Function for initial time ------------------ //
+updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal);
+
+// ------------------ Resize plot when window size is changed ------------------ //
+addEventListener("resize", (event) => {updateFunction(initialPositionSliderVal, initialVelocitySliderVal, constantAccelerationSliderVal, timeMaxSliderVal);});
